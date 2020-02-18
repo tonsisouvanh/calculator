3615c39b0a616aa52c2325f88c54449804a0457b
tonsisouvanh <38555090+tonsisouvanh@users.noreply.github.com>
2020-02-18 10:47:54 +0700

    Add files via upload

---
+++ cal/calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a + b = " + add(a, b));
+        }
+        public static int add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
