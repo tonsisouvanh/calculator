56a60617211d15b57dd6f96b7798280bff739afe
Tons <tonsisouvanh7@gmail.com>
2020-02-18 10:55:11 +0700

    minus function

--- cal/calculator/Program.cs
+++ cal/calculator/Program.cs
@@ -10,10 +10,16 @@ namespace calculator
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             Console.WriteLine("a + b = " + add(a, b));
+            Console.WriteLine("a - b = " + minus(a, b));
+
         }
         public static int add(int a,int b)
         {
             return a + b;
+        }
+         public static int minus(int a,int b)
+        {
+            return a - b;
         }
     }
 }
